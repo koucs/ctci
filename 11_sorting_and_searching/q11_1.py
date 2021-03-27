2ffed4780f97f5f98b1f36dff79e48a249aa98d8
kou_cs <kou.cshu@gmail.com>
2021-03-28 00:11:48 +0900

    11.1 merge (text solution)

--- 11_sorting_and_searching/q11_1.py
+++ 11_sorting_and_searching/q11_1.py
@@ -22,6 +22,27 @@ def merge(arr_a: List, arr_b: List) -> List:
     return ans
 
 
+def merge_text_solution(arr_a: List, arr_b: List, last_a: int, last_b: int):
+    idx_a = last_a - 1
+    idx_b = last_b - 1
+    idx_merged = last_a + last_b - 1
+    while idx_a >= 0 and idx_b >= 0:
+        if arr_a[idx_a] >= arr_b[idx_b]:
+            arr_a[idx_merged] = arr_a[idx_a]
+            idx_a -= 1
+        else:
+            arr_a[idx_merged] = arr_b[idx_b]
+            idx_b -= 1
+        idx_merged -= 1
+
+    while idx_b >= 0:
+        arr_a[idx_merged] = arr_b[idx_b]
+        idx_b -= 1
+        idx_merged -= 1
+
+    return arr_a
+
+
 class Test(unittest.TestCase):
     def test_1(self):
         a = [33, 44, 55, 66, 88, 99, None, None, None]
@@ -33,6 +54,21 @@ class Test(unittest.TestCase):
         a = [9, 10, 11, 12, 13, None, None, None, None]
         b = [4, 5, 6, 7]
         self.assertEqual(merge(a, b), [4, 5, 6, 7, 9, 10, 11, 12, 13])
+        return
+
+    def test_2(self):
+        a = [33, 44, 55, 66, 88, 99, None, None, None]
+        b = [11, 22, 77]
+        last_a, last_b = len(a) - a.count(None), len(b)
+        self.assertEqual(merge_text_solution(a, b, last_a, last_b), [11, 22, 33, 44, 55, 66, 77, 88, 99])
+        a = [11, 22, 55, 66, 88, None, None, None]
+        b = [33, 44, 99]
+        last_a, last_b = len(a) - a.count(None), len(b)
+        self.assertEqual(merge_text_solution(a, b, last_a, last_b), [11, 22, 33, 44, 55, 66, 88, 99])
+        a = [9, 10, 11, 12, 13, None, None, None, None]
+        b = [4, 5, 6, 7]
+        last_a, last_b = len(a) - a.count(None), len(b)
+        self.assertEqual(merge_text_solution(a, b, last_a, last_b), [4, 5, 6, 7, 9, 10, 11, 12, 13])
         return
 
 
